825a2ab3cb4e34e4c2fc49aadd6caefcae38827b
Mehmet Adem Sengul <masengul@gmail.com>
2014-04-05 18:55:22 +0300

    initialize commit

---
+++ src/main/java/com/bilisimegitim/deneme/IndexBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bilisimegitim.deneme;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Adem
+ */
+@ManagedBean
+@RequestScoped
+public class IndexBean {
+
+    /**
+     * Creates a new instance of IndexBean
+     */
+    public IndexBean() {
+    }
+    
+}
